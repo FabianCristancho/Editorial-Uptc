adf15bd075e729786398338f8a3945971ad0053a
FabianCristancho <fabiancrisrincon@outlook.com>
2020-12-01 09:12:08 -0500

    Header social media creation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PageEdUptcComponent } from './page-ed-uptc/page-ed-uptc.component';
+import { NavBarEncabezado1Component } from './page-ed-uptc/nav-bar-encabezado1/nav-bar-encabezado1.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PageEdUptcComponent,
+    NavBarEncabezado1Component
   ],
   imports: [
     BrowserModule,
