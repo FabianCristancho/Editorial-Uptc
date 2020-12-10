5cc821a2d80abde2dcf75334e578017ab5e8d721
FabianCristancho <fabiancrisrincon@outlook.com>
2020-12-10 10:11:03 -0500

    Tests files configured

--- src/app/page-ed-uptc/page-ed-uptc.component.spec.ts
+++ src/app/page-ed-uptc/page-ed-uptc.component.spec.ts
@@ -1,6 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PageEdUptcComponent } from './page-ed-uptc.component';
+import { BannerUptcComponent } from "./banner-uptc/banner-uptc.component";
+import { FastNavigationComponent } from "./fast-navigation/fast-navigation.component";
+import { FooterComponent } from "./footer/footer.component";
+import { NavBarEncabezado1Component } from "./nav-bar-encabezado1/nav-bar-encabezado1.component";
+import { NewsAndEventsComponent } from "./news-and-events/news-and-events.component";
 
 describe('PageEdUptcComponent', () => {
   let component: PageEdUptcComponent;
@@ -8,7 +13,7 @@ describe('PageEdUptcComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ PageEdUptcComponent ]
+      declarations: [ PageEdUptcComponent, BannerUptcComponent, FastNavigationComponent, FooterComponent, NavBarEncabezado1Component, NewsAndEventsComponent ]
     })
     .compileComponents();
   });
